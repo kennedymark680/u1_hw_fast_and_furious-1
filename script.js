340ecaa6be26a9e6e3bcfb01ca8de81391429bed
Mark Kennedy <kennedymark@hotmail.com>
2022-03-03 19:41:52 -0500

    attempting to refork for a pull request after SAVING THE FILE.

--- script.js
+++ script.js
@@ -4,32 +4,54 @@ let quotes = [
   `You can have any brew you want... as long as it's a Corona.`,
   `You almost had me? You never had me - you never had your car!`,
   `I don't have friends. I have family.`,
-  `It don't matter if you win by an inch or a mile. Winning's winning.`,
-];
+  `It don't matter if you win by an inch or a mile. Winning's winning.`
+]
 
-window.onload =  function(event) {
-  
+window.onload = function (event) {
   // Random quote of the day generator
-  const randomQuote = function() {
+  const randomQuote = function () {
     document.querySelector('#quote-of-the-day').textContent = `"${
       quotes[Math.floor(Math.random() * quotes.length)]
-    }"`;
-  };
-  randomQuote();
+    }"`
+  }
+  randomQuote()
 
   // Do all of your work inside the window.onload function (in other words, here!)
 
   // Part 1
+  document.getElementById('main-title').innerText = "Hi there I'm DOM Toretto"
 
   // Part 2
+  document.querySelector('body').style.backgroundColor = 'blue'
 
   // Part 3
+  document
+    .querySelector('#favorite-things')
+    .childNodes[11].remove()
 
-  // Part 4
+    // Part 4
+    .querySelectorAll('.special-title')
+    .forEach((elm) => (elm.style.fontSize = '2rem'))
 
   // Part 5
+  document.querySelector('#past-races').childNodes[7].remove()
 
   // Part 6
+  const newItem = document.createElement('li')
+  newItem.innerText = 'Philly'
+  document.querySelector('#past-races').appendChild(newItem)
 
   // Part 7
+  const newDiv = document.createElement('div')
+  const newH1 = document.createElement('h1')
+  const newP = document.createElement('p')
+
+  newDiv.setAttribute('class', 'blog-post purple')
+  newH1.innerText = 'Philly'
+  newP.innerText = 'I dont know what to say'
+
+  newDiv.appendChild(newH1)
+  newDiv.appendChild(newP)
+
+  document.querySelector('.main').appendChild(newDiv)
 }
